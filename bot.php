489604f5ecd1599dfff7fdecd8f8f0831fca4cfb
Masaaki Morishita <ikasama3.0@gmail.com>
2015-01-13 09:31:38 +0900

    first implement

---
+++ bot.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+const CONSUMER_KEY = '';
+const CONSUMER_SECRET = '';
+const TOKEN = '';
+const TOKEN_SECRET = '';
+
+$client = new TwitterOAuth(
+    CONSUMER_KEY,
+    CONSUMER_SECRET,
+    TOKEN,
+    TOKEN_SECRET
+);
+
+$deadline = new DateTime('2015-02-09'); // ??
+$today = new DateTime(date('Y-m-d'));
+$rest_day = $d2->diff($d1)->format('%a日');
+$text = '修論提出まであと' . $rest_day . '！！';
+
+// random tweet text with rest time
+$tweets = [
+    ""
+];
+
+$params = [
+    'status' => $text . $tweets[array_rand($tweets)]
+];
+
+$client->post("statuses/update", $params);
